f17ccd0940edd81fd13bbb862d815a30dad2573b
zxc337 <zhangxiongcai337@163.com>
2016-09-28 13:58:04 +0800

    .

--- src/main/java/com/msun/wxbet/support/WebAppConfig.java
+++ src/main/java/com/msun/wxbet/support/WebAppConfig.java
@@ -9,7 +9,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,7 +19,7 @@ import com.msun.wxbet.support.interceptor.WechatJsApiInterceptor;
 /**
  * @author zxc Feb 22, 2016 8:36:05 PM
  */
-@EnableRedisHttpSession
+// @EnableRedisHttpSession
 @EnableScheduling
 @EnableAutoConfiguration
 @ComponentScan("com.msun")
