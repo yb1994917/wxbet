6a6808fc8dcb9816851609ba82e6e6418cfc293b
zxc337 <zhangxiongcai337@163.com>
2016-10-14 16:49:30 +0800

    .

---
+++ src/main/java/com/msun/wxbet/controller/InviteController.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2015-2020 uuzu.com All right reserved.
+ */
+package com.msun.wxbet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author zxc Oct 14, 2016 12:30:12 PM
+ */
+@Controller
+@RequestMapping(value = "/invite")
+public class InviteController {
+
+}
